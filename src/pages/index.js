0bc596509c85691cec99b9b7369ecc0925def8e0
ginotria <romanangelo.tria@gmail.com>
2017-11-01 00:16:39 +0800

    * netlify admin config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Helmet from 'react-helmet';
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+class IndexPage extends React.Component {
+  componentDidMount() {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", user => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          });
+        }
+      });
+    }
+  }
+  render() {
+    return(
+      <div>
+        <Helmet>
+          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        </Helmet>
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    );
+  }
+}
 
 export default IndexPage
